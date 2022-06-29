499b275c7c4524bc786e1a42024afa6cf8f5852b
Andrei Hoszu <andrei.hoszu@softwire.com>
2022-06-29 09:40:43 +0100

    added classes

--- supportbank/Program.cs
+++ supportbank/Program.cs
@@ -16,14 +16,12 @@ namespace supportbank
     {
         private static readonly ILogger logger = LogManager.GetCurrentClassLogger();
 
-        static String[] readDataFromFiles(String[] paths)
+        static Dictionary<String, Account> parseDataFromFiles(String[] paths)
         {
-            //TODO
             String[] data = { };
 
             foreach(String path in paths)
             {
-                Console.WriteLine("file number 1?");
                 String format = path.Substring(path.LastIndexOf("."));
 
                 switch(format)
@@ -32,19 +30,72 @@ namespace supportbank
                         data = data.Concat(readDataFromCVS(path)).ToArray();
                         break;
                     case ".json":
+                        //dates will be converted to CVS format: day/month/year
                         data = data.Concat(readDataFromJSON(path)).ToArray();
                         break;
                     case ".xml":
+                        //dates will be converted to CVS format: day/month/year
                         data = data.Concat(readDataFromXML(path)).ToArray();
                         break;
                     default:
                         Console.WriteLine("Unsuported file type " + format);
-                        logger.Info("Program tried to read a file that it does not support.");
+                        logger.Info("Program tried to read a file that is not supported.");
                         break;
                 }
             }
 
-            return data;
+            Dictionary<String, Account> accounts = new Dictionary<String, Account>();
+
+            foreach (String line in data)
+            {
+                String[] lineData = line.Split(",");
+
+                //checks the first account
+                if(!accounts.ContainsKey(lineData[1]))
+                {
+                    accounts.Add(lineData[1], new Account(lineData[1]));
+                }
+
+                try
+                {
+                    accounts[lineData[1]].TotalAmount -= Double.Parse(lineData[4]);
+                    int day = Int32.Parse(lineData[0].Substring(0, 2));
+                    int month = Int32.Parse(lineData[0].Substring(3, 2));
+                    int year = Int32.Parse(lineData[0].Substring(6, 4));
+                    DateTime parsedDate = new DateTime(year, month, day);
+                    Double parsedAmount = Double.Parse(lineData[4]);
+                    accounts[lineData[1]].addTransaction(new Transaction(parsedDate, lineData[1], lineData[2], lineData[3], parsedAmount));
+                }
+                catch (Exception e)
+                {
+                    logger.Debug("Uh oh, something happened: the program tried to add a transaction and it failed.");
+                    logger.Error("Error:\n" + e + "\n\n"+line+"\n\n");
+                }
+
+                //checks the second account
+                if(!accounts.ContainsKey(lineData[2]))
+                {
+                    accounts.Add(lineData[2], new Account(lineData[1]));
+                }
+
+                try
+                {
+                    accounts[lineData[2]].TotalAmount = Double.Parse(lineData[4]);
+                    int day = Int32.Parse(lineData[0].Substring(0, 2));
+                    int month = Int32.Parse(lineData[0].Substring(3, 2));
+                    int year = Int32.Parse(lineData[0].Substring(6, 4));
+                    DateTime parsedDate = new DateTime(year, month, day);
+                    Double parsedAmount = Double.Parse(lineData[4]);
+                    accounts[lineData[2]].addTransaction(new Transaction(parsedDate, lineData[1], lineData[2], lineData[3], parsedAmount));
+                }
+                catch (Exception e)
+                {
+                    logger.Debug("Uh oh, something happened: the program tried to add a transaction and it failed.");
+                    logger.Error("Error:\n" + e + "\n\n" + line + "\n\n");
+                }
+            }
+
+            return accounts;
         }
 
         static String[] readDataFromCVS(String path)
@@ -62,7 +113,7 @@ namespace supportbank
                 return null;
             }
 
-            return data.Split("\n").Skip(1).SkipLast(1).ToArray(); ;
+            return data.Split("\n").Skip(1).SkipLast(1).ToArray();
         }
 
         static String[] readDataFromJSON(String path)
@@ -85,7 +136,16 @@ namespace supportbank
 
             foreach(JToken transaction in transactions)
             {
-                returnData[counter++] = transaction["Date"] + "," + transaction["FromAccount"] + "," + transaction["ToAccount"] + "," + transaction["Narrative"] + "," + transaction["Amount"];
+                //we need to convert from ISO-8601 date(YYYY-MM-DDTHH-MM-SS.MMMZ) format to DD/MM/YYYY
+                //by doing this we will lose the timestamps
+                //but this is not a big deal since the other date formats only count up to days and not miliseconds
+                //or we could do the other way around and add midnight timestamps to all other datetimes
+                String convertedDate = (transaction["Date"] + "").Substring(0, 10); ;//it is really strange really
+
+                //                     days                   /             months                  /               years
+                //convertedDate = convertedDate.Substring(8) + "/" + convertedDate.Substring(4, 2) + "/" + convertedDate.Substring(0, 4);
+
+                returnData[counter++] = convertedDate + "," + transaction["FromAccount"] + "," + transaction["ToAccount"] + "," + transaction["Narrative"] + "," + transaction["Amount"];
             }
 
             return returnData;
@@ -108,87 +168,41 @@ namespace supportbank
             XmlDocument doc = new XmlDocument();
             doc.LoadXml(data);
 
-            //XmlSerializer
-
             String[] returnData = new String[doc.LastChild.ChildNodes.Count];
             int counter = 0;
 
             foreach (XmlNode node in doc.LastChild.ChildNodes)
             {
-                returnData[counter++] = node.Attributes["Date"]?.InnerText + "," + node.ChildNodes[2].ChildNodes[0].InnerText + "," + node.ChildNodes[2].ChildNodes[1].InnerText + "," + node.ChildNodes[0].InnerText + "," + node.ChildNodes[1].InnerText;
+                String convertedDate = node.Attributes["Date"]?.InnerText;
+                DateTime temp = DateTime.FromOADate(Double.Parse(convertedDate));
+                String day = temp.Day + "";
+                if(day.Length == 1)
+                {
+                    day = "0" + day;
+                }
+                String month = temp.Month + "";
+                if(month.Length == 1)
+                {
+                    month = "0" + month;
+                }
+
+                convertedDate = day + "/" + month + "/" + temp.Year;
+
+                returnData[counter++] = convertedDate + "," + node.ChildNodes[2].ChildNodes[0].InnerText + "," + node.ChildNodes[2].ChildNodes[1].InnerText + "," + node.ChildNodes[0].InnerText + "," + node.ChildNodes[1].InnerText;
             }
 
             return returnData;
         }
 
-        static Dictionary<String, double> calculateTransactions(Dictionary<String, double> accountList, String[] dataLines)
+        static void displayStatus(Dictionary<String, Account> accountList, String key)
         {
-            foreach (String line in dataLines)
+            if(accountList[key].TotalAmount > 0)
             {
-                String[] lineData = line.Split(",");
-
-                if (accountList.ContainsKey(lineData[1]))
-                {
-                    try
-                    {
-                        accountList[lineData[1]] -= Double.Parse(lineData[4]);
-                    }
-                    catch (Exception e)
-                    {
-                        logger.Debug("Uh oh, something happened: the program tried to convert something that wasn't a number into a variable of type Double.");
-                        logger.Error("Error:\n" + e);
-                    }
-                }
-                else
-                {
-                    try
-                    {
-                        accountList.Add(lineData[1], -Double.Parse(lineData[4]));
-                    }
-                    catch (Exception e)
-                    {
-                        logger.Debug("Uh oh, something happened: the program tried to convert something that wasn't a number into a variable of type Double.");
-                        logger.Error("Error:\n" + e);
-                    }
-                }
-                if (accountList.ContainsKey(lineData[2]))
-                {
-                    try
-                    {
-                        accountList[lineData[2]] += Double.Parse(lineData[4]);
-                    }
-                    catch (Exception e)
-                    {
-                        logger.Debug("Uh oh, something happened: the program tried to convert something that wasn't a number into a variable of type Double.");
-                        logger.Error("Error:\n" + e);
-                    }
+                Console.WriteLine(key + " is owed " + accountList[key].TotalAmount);
             }
-                else
-                {
-                    try
-                    {
-                        accountList.Add(lineData[2], +Double.Parse(lineData[4]));
-                    }
-                    catch (Exception e)
-                    {
-                        logger.Debug("Uh oh, something happened: the program tried to convert something that wasn't a number into a variable of type Double.");
-                        logger.Error("Error:\n" + e);
-                    }
-                }
-            }
-
-            return accountList;
-        }
-
-        static void displayStatus(Dictionary<String, double> accountList, String key)
-        {
-            if (accountList[key] > 0)
+            else if(accountList[key].TotalAmount < 0)
             {
-                Console.WriteLine(key + " is owed " + accountList[key]);
-            }
-            else if (accountList[key] < 0)
-            {
-                Console.WriteLine(key + " owes " + accountList[key]);
+                Console.WriteLine(key + " owes " + accountList[key].TotalAmount);
             }
             else
             {
@@ -196,9 +210,9 @@ namespace supportbank
             }
         }
 
-        static void displayTransactions(Dictionary<String, double> accountList, String[] dataLines, String accountName)
+        static void displayTransactions(Dictionary<String, Account> accountList, String accountName)
         {
-            if (accountList.ContainsKey(accountName))
+            if(accountList.ContainsKey(accountName))
             {
                 Console.WriteLine("Account found.");
 
@@ -206,17 +220,15 @@ namespace supportbank
 
                 Console.WriteLine("List of transactions:");
 
-                foreach (String line in dataLines)
+                foreach(Transaction transaction in accountList[accountName].transactions)
                 {
-                    String[] lineData = line.Split(",");
-
-                    if (lineData[1] == accountName)
+                    if(transaction.From == accountName)
                     {
-                        Console.WriteLine(accountName + " sent " + lineData[4] + " to " + lineData[2] + " on " + lineData[0] + "; Narrative: " + lineData[3]);
+                        Console.WriteLine(accountName + " sent " + transaction.Amount + " to " + transaction.To + " on " + transaction.Date + "; Narrative: " + transaction.Narrative);
                     }
-                    else if (lineData[2] == accountName)
+                    else if(transaction.To == accountName)
                     {
-                        Console.WriteLine(accountName + " received " + lineData[4] + " from " + lineData[1] + " on " + lineData[0] + "; Narrative: " + lineData[3]);
+                        Console.WriteLine(accountName + " received " + transaction.Amount + " from " + transaction.From + " on " + transaction.Date + "; Narrative: " + transaction.Narrative);
                     }
                 }
             }
@@ -234,23 +246,16 @@ namespace supportbank
             config.AddTarget("File Logger", target);
             config.LoggingRules.Add(new LoggingRule("*", LogLevel.Debug, target));
             LogManager.Configuration = config;
-
             logger.Info("Program started. Hooray!");
-            Dictionary<String, double> accountList = new Dictionary<string, double>();
 
             String[] paths = { @"C:\Work\Training\supportbank\supportbank\Transactions2014.csv", @"C:\Work\Training\supportbank\supportbank\DodgyTransactions2015.csv" ,
             @"C:\Work\Training\supportbank\supportbank\Transactions2013.json", @"C:\Work\Training\supportbank\supportbank\Transactions2012.xml"};
-
-            String[] dataLines = readDataFromFiles(paths);
-
+            Dictionary<String, Account> accountList = parseDataFromFiles(paths);
             logger.Info("Program successfully read the data from the specified files.");
-
-            accountList = calculateTransactions(accountList, dataLines);
 
             logger.Info("Program successfully calculated the transactions.");
 
             Console.WriteLine("List All or List [Account]?");
-
             String userInput = Console.ReadLine();
             List<String> keyList = new List<String>(accountList.Keys);
 
@@ -266,7 +271,7 @@ namespace supportbank
             String accountName = String.Join(" ", userInput.Split(" ").Skip(1).ToArray());
             if(accountName != "All")
             {
-                displayTransactions(accountList, dataLines, accountName);
+                displayTransactions(accountList, accountName);
                 logger.Info("Successfully displayed account's status.");
             }
 
